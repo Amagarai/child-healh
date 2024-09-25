eeafa8eebcb27b13056fc10efbd381f6316087c0
Amagaraï Guindo <amagarai6@gmail.com>
2024-09-25 05:43:34 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'child-healh',
+  webDir: 'www'
+};
+
+export default config;
